36cc378c95f646b2ceaea6e4f4591e8c3145a63e
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-07-06 20:54:20 +0530

    Add message send function

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -14,7 +14,6 @@ export const getUsersForSidebar = async (req, res) => {
 	}
 };
 
-
 // gets the messages beteeen two users
 export const getMessages = async (req, res) => {
 	try {
@@ -35,7 +34,32 @@ export const getMessages = async (req, res) => {
 	}
 };
 
-
 export const sendMessage = async (req, res) => {
-  
-}
+	try {
+		const { text, image } = req.body;
+		const { id: receiverId } = req.params;
+		const senderId = req.user._id;
+
+		let imageUrl;
+		if (image) {
+			const uploadedImage = await cloudinary.uploader.upload(image);
+			imageUrl = uploadedImage.secure_url;
+		}
+
+		const newMessage = new Message({
+			senderId,
+			receiverId,
+			text,
+			image: imageUrl,
+		});
+
+		await newMessage.save();
+
+		// todo: realtime functionality goes here => socket.io
+
+		res.status(201).json(newMessage);
+	} catch (error) {
+		console.error("Error in sendMessage controller:", error.message);
+		res.status(500).json({ message: "Internal server error" });
+	}
+};
