4acbd403338bfef198db3ca5dca307121ae22457
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-07-06 20:20:30 +0530

    Add profile pic support

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+// protect route for checking if user is logged in (middleware)
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
