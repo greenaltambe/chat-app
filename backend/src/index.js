765d9db3f80b3f5fe8f20c0416abebd19e1320cd
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-07-06 20:48:55 +0530

    Create getUserForSidebar, getMessages

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -15,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
 	console.log("Server is running on PORT:" + PORT);
