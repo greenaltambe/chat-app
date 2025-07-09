3a01f3c33dc911de99482edfe9a235cbd409d9b9
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-07-09 19:56:31 +0530

    Fix syntax:

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,13 +16,12 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
 		origin: "http://localhost:5173",
-		credentials: true, // Allow cookies to be sent with requests
+		credentials: true,
 	})
 );
 
@@ -32,12 +31,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-	app.get("*", (req, res) => {
+	app.get(/(.*)/, (req, res) => {
 		res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-	})
+	});
 }
 
 server.listen(PORT, () => {
-	console.log("Server is running on PORT:" + PORT);
+	console.log("server is running on PORT:" + PORT);
 	connectDB();
 });
